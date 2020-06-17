2bce4f28706bff6a09057244593545be21b1e40b
azarb <38533949+azarb@users.noreply.github.com>
2020-06-18 01:15:46 +0430

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import './App.css';
+import Game from './components/Game';
+
+function App() {
+  return (
+   <Game />
+  );
+}
+
+export default App;
